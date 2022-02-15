f64b1b34779a5d9cd55931d12afbc30a40122baf
ABIOYE OBALOLUWA <horbarz007@gmail.com>
2022-02-15 14:22:57 +0100

    Updated on Development branch

--- src/components/home/Hero_two.js
+++ src/components/home/Hero_two.js
@@ -61,15 +61,53 @@ export const Hero_two = (props) => {
                 </form>
                 <form id='step_2'>
                     <div>
-                        <textarea placeholder='What are your goals for this copy?' />
+                        <textarea rows={4} placeholder='What are your goals for this copy?' />
                         <span>                      
                             <FontAwesomeIcon icon={faQuestionCircle} className={props.icon_color ? 'orange_question' : 'blue_question'}/>
                         </span>
                     </div>
                     <div>
-                        <input type="text" placeholder='who is your target audience for this copy?' />
+                        <textarea rows={3} placeholder='who is your target audience for this copy?' />
                     </div>
                 </form>
+                <form id='step_3'>
+                    <div>
+                        <textarea rows={4} placeholder='Other details' />
+                        <span>                      
+                            <FontAwesomeIcon icon={faQuestionCircle} className={props.icon_color ? 'orange_question' : 'blue_question'}/>
+                        </span>
+                    </div>
+                    <label> 
+                        <input type="file" hidden/>
+                        <div className='file_btn'>
+                            Upload additional files
+                            <span>Upload</span>
+                        </div>
+                    </label>
+                </form>
+                <div className='final_step'>
+                    <h5>Thank you for <span>Patronizing Us</span></h5>
+                    <p>Make choices that best work for you</p>
+                    <div className='get_call'>
+                        <img className='final_step_icons' src={process.env.PUBLIC_URL + "/assets/icons/icon/call_icon.svg"} />
+                        <h3>Get Quote</h3>
+                        <img src={process.env.PUBLIC_URL + "/assets/icons/icon/arrow_right.svg"} />
+                    </div>
+                    <div className='get_call' style={{"marginTop":"20px"}}>
+                        <img className='final_step_icons' src={process.env.PUBLIC_URL + "/assets/icons/icon/quote_icon.svg"}/>
+                        <h3>Book A Call</h3>
+                        <img className='arrow' src={process.env.PUBLIC_URL + "/assets/icons/icon/arrow_right.svg"} />
+                    </div>
+                </div>
+                <div className='final_img'>
+                    <img src={process.env.PUBLIC_URL + "/assets/icons/icon/final_img.svg"} />
+                    <div className='hh'>
+                        <h4><span><img src={process.env.PUBLIC_URL + "/assets/icons/icon/final_checkbox.svg"} /></span>all done</h4>
+                        <p>Check Your <span>Email</span> for follow up</p>
+                        <p>See yaa!!! soon</p>
+                    </div>
+                   
+                </div>
             </div>
 
         </div>
